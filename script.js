b4ddad71585820cf0631d9be4f39e485edeba250
Anas Chebili <anaschebili@gmail.com>
2025-04-14 20:48:22 +0200

    set up game state and DOM caching

---
+++ script.js
@@ -0,0 +1,8 @@
+const state = {
+    playerScore: 0,
+    computerScore: 0,
+};
+
+const playerScoreEl = document.getElementById('player-score');
+const computerScoreEl = document.getElementById('computer-score');
+const controlBtns = document.querySelectorAll('.control-btn');
